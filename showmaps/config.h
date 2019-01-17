f14352650595b065c2af16cece85c4dc43bf1411
lyk <you@example.com>
2019-01-17 17:48:16 +0800

    increase max file size allowed

--- showmaps/config.h
+++ showmaps/config.h
@@ -139,7 +139,7 @@
 
 /* Maximum size of input file, in bytes (keep under 100MB): */
 
-#define MAX_FILE            8192
+#define MAX_FILE            (10 * 1024 * 1024)
 
 /* The same, for the test case minimizer: */
 
